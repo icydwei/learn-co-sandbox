2535ee5e7ddf1d5f2f4b140c0d19691a31552d10
Rob Paik <robpaik@gmail.com>
2019-07-27 13:28:08 +0000

    Automatically backed up by Learn

---
+++ learn-ruby.rb
@@ -0,0 +1,9 @@
+#run_code_inside = FALSE
+#puts "Code before if...end"
+#if run_code_inside
+#  puts "code inside"
+#end
+#puts "Code after if...end"
+
+this_year = Time.now.year
+puts "Hey, it's 2019!" if this_year == 2019
